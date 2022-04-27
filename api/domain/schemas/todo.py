3a3a6867e5193f4693a0c67cedb6b962e4a09ea0
Ravi Prasad Velamuri Venkata <vravip@hotmail.com>
2022-04-27 14:54:06 +0530

    Made the changes for completing Assignment

--- api/domain/schemas/todo.py
+++ api/domain/schemas/todo.py
@@ -2,17 +2,17 @@ from api.domain.schemas.schema_base import SchemaBase
 
 
 class TodoBase(SchemaBase):
-    """your code goes here"""
-
+    id: int
+    description: str
+    user_id: int
 
 class TodoCreate(TodoBase):
     pass
 
-
 class Todo(TodoBase):
+    #user_id: int
     class Config:
         orm_mode = True
 
-
-class TodoResponse(Todo):
+class TodoResponse(Todo): 
     pass
